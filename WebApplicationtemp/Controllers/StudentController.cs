578a46104972035c0ab78c226e583075f0d72863
Anamika-s <anamika.sawhney22@gmail.com>
2023-06-19 13:49:34 +0530

    Add project files.

---
+++ WebApplicationtemp/Controllers/StudentController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplicationtemp.Controllers
+{
+   
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        public IActionResult Get()
+        {
+            List<string> list = new List<string>()
+            { "aaa", "bbb"};
+            return Ok(list);
+        }
+    }
+}
